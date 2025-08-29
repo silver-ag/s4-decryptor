ab92a5e5d8b3934ea041cbe7fba4ee7dac02a87f
silver <asaglenister@protonmail.com>
2025-08-29 20:37:56 +0100

    basic functionality

---
+++ src/decryptor.py
@@ -0,0 +1,172 @@
+from PyQt6.QtCore import QSize, Qt
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QScrollArea, QPlainTextEdit, QPushButton, QMessageBox
+from PyQt6.QtGui import QColor, QPalette
+
+import shamirs
+import json
+import itertools
+
+PROJECT_NAME = '[projectname]'
+# BG_COLOUR = QColor('black')
+# TEXT_COLOUR = QColor('green')
+
+shamirs.share.__eq__ = lambda self, other: self.index == other.index and self.value == other.value and self.modulus == other.modulus
+
+# Subclass QMainWindow to customize your application's main window
+class DecryptorMainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.keys = {} # key: keybox
+        self.secrets = {} # secret: secretbox
+
+        self.setWindowTitle(f'{PROJECT_NAME} decryptor')
+        top_layout = QVBoxLayout()
+        self.secrets_layout = QHBoxLayout()
+        secrets_widget = Colour('red')
+        secrets_widget.setLayout(self.secrets_layout)
+        secrets_scroll_widget = QScrollArea()
+        secrets_scroll_widget.setWidget(secrets_widget)
+        secrets_scroll_widget.setWidgetResizable(True)
+        top_layout.addWidget(secrets_scroll_widget)
+        self.keys_layout = QHBoxLayout()
+        self.keys_layout.addWidget(AddKeyBox())
+        keys_widget = Colour('blue')
+        keys_widget.setLayout(self.keys_layout)
+        keys_scroll_widget = QScrollArea()
+        keys_scroll_widget.setWidget(keys_widget)
+        keys_scroll_widget.setWidgetResizable(True)
+        top_layout.addWidget(keys_scroll_widget)
+        
+        central_widget = QWidget()
+        central_widget.setLayout(top_layout)
+        # Set the central widget of the Window.
+        self.setCentralWidget(central_widget)
+
+    def addSecret(self, secret):
+        new_secret_box = SecretBox(secret)
+        self.secrets_layout.addWidget(new_secret_box)
+        self.secrets[secret] = new_secret_box
+
+    def addKey(self, key_string):
+        try:
+            key_data = json.loads(key_string)
+            key = shamirs.share(index = key_data['index'],
+                                value = key_data['value'],
+                                modulus = key_data['modulus'])
+        except Exception as e:
+            self.messageBox(f'not a valid {PROJECT_NAME} key', 'invalid key')
+            return False
+
+        print(key)
+        for other_key in self.keys:
+            print(other_key)
+            if other_key == key:
+                self.messageBox('you already have that key', 'key already present')
+                return False
+        
+        self.checkForSecrets(key)
+        new_key_box = KeyBox(key)
+        self.keys_layout.insertWidget(self.keys_layout.count()-1, new_key_box)
+        self.keys[key] = new_key_box
+
+        return True
+
+    def removeKey(self, key):
+        self.keys_layout.removeWidget(self.keys[key])
+        self.keys[key].deleteLater()
+        self.keys.pop(key)
+        # TODO: remove any secret that depended on it
+
+    def removeSecret(self, secret):
+        self.secrets_layout.removeWidget(self.secrets[secret])
+        self.secrets[secret].deleteLater()
+        self.secrets.pop(secret)
+
+    def checkForSecrets(self, new_key):
+        for quantity in range(len(self.keys)+1):
+            for combo in itertools.combinations(self.keys, quantity):
+                print(f"checking {[share.index for share in list(combo) + [new_key]]}")
+                try:
+                    new_secret = shamirs.interpolate(list(combo) + [new_key])
+                    self.addSecret(str(new_secret))
+                    print(f"found {new_secret}")
+                except Exception as e:
+                    print(e)
+                    pass
+
+    def messageBox(self, message, title):
+        dlg = QMessageBox(self)
+        dlg.setWindowTitle(title)
+        dlg.setText(message)
+        dlg.exec()
+            
+
+class SecretBox(QWidget):
+    def __init__(self, secret):
+        super().__init__()
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor('black'))
+        self.setPalette(palette)
+        layout = QVBoxLayout()
+        layout.addWidget(QLabel(str(secret)))
+        self.setLayout(layout)
+
+class KeyBox(QWidget):
+    def __init__(self, key):
+        super().__init__()
+        self.key = key
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor('black'))
+        self.setPalette(palette)
+        x_button = QPushButton('X')
+        x_button.setCheckable(True)
+        x_button.clicked.connect(self.delete)
+        layout = QVBoxLayout()
+        layout.addWidget(x_button)
+        layout.addWidget(QLabel(str(key)))
+        self.setLayout(layout)
+    def delete(self):
+        self.parent().parent().parent().parent().parent().removeKey(self.key)
+
+class AddKeyBox(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor('black'))
+        self.setPalette(palette)
+        add_button = QPushButton('+')
+        add_button.setCheckable(True)
+        add_button.clicked.connect(self.add_button_pressed)
+        self.text_box = QPlainTextEdit()
+        layout = QVBoxLayout()
+        layout.addWidget(self.text_box)
+        layout.addWidget(add_button)
+        self.setLayout(layout)
+    def add_button_pressed(self):
+        if self.parent().parent().parent().parent().parent().addKey(self.text_box.toPlainText()):
+            self.text_box.clear()
+
+class Colour(QWidget):
+    def __init__(self, color):
+        super().__init__()
+        self.setMinimumSize(QSize(10,10))
+        self.setAutoFillBackground(True)
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+
+def make_json_shares():
+    shares = shamirs.shares(3, 3, 10)
+    print([f'{{"index": {share.index}, "value": {share.value}, "modulus": {share.modulus} }}' for share in shares])
+
+app = QApplication([])
+
+window = DecryptorMainWindow()
+window.show()
+
+app.exec()
